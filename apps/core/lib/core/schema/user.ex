cbd6e252f13318eff7fa6e0fc7f6a5128b5a80a6
Nicolas Mena <nmena.garzon@gmail.com>
2023-11-25 20:55:05 -0500

    Add hash password field to users (#3)

--- apps/core/lib/core/schema/user.ex
+++ apps/core/lib/core/schema/user.ex
@@ -2,9 +2,12 @@ defmodule GoEscuelaLms.Core.Schema.User do
   @moduledoc """
   This module represents the audit schema. Audits are the read models for events.
   """
+
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias __MODULE__
+  alias GoEscuelaLms.Core.Repo, as: Repo
   alias GoEscuelaLms.Core.Schema.{Enrollment}
 
   @primary_key {:uuid, Ecto.UUID, autogenerate: true}
@@ -14,15 +17,32 @@ defmodule GoEscuelaLms.Core.Schema.User do
     field(:email, :string)
     field(:birth_date, :date)
     field(:role, :string)
-
+    field(:password_hash, :string)
     has_many(:enrollments, Enrollment, foreign_key: :user_id)
 
     timestamps()
   end
 
-  def changeset(course, attrs) do
-    course
-    |> cast(attrs, [:full_name, :email, :birth_date, :role])
-    |> validate_required([:full_name])
+  def create(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
   end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:full_name, :email, :birth_date, :role, :password_hash])
+    |> validate_required([:full_name, :password_hash])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> validate_length(:email, max: 160)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password_hash: password_hash}} = changeset
+       ) do
+    change(changeset, password_hash: Bcrypt.hash_pwd_salt(password_hash))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
---
+++ apps/core/priv/repo/migrations/20231125235434_users_add_hash_password_column.exs
@@ -0,0 +1,9 @@
+defmodule GoEscuelaLms.Core.Repo.Migrations.UsersAddHashPasswordColumn do
+  use Ecto.Migration
+
+  def change do
+    alter table("users") do
+      add :password_hash, :string, null: false
+    end
+  end
+end
