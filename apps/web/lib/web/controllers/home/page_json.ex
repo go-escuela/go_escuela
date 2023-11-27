7fc70ce06b9f59b92761cf10f74c1255107b909c
Nicolas Mena <nmena.garzon@gmail.com>
2023-11-26 23:23:41 -0500

    Add web app (#5)
    * Add web app
    
    * add home page controller

---
+++ apps/web/lib/web/controllers/home/page_json.ex
@@ -0,0 +1,8 @@
+defmodule Web.Home.PageJSON do
+  @doc """
+  Renders a single user.
+  """
+  def show(_params) do
+    %{status: :ok}
+  end
+end
--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,12 @@
 import Config
 
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :web, Web.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "t2MLXkDJ2CQ91+yN67qU+nuBSqT94xgK9KcYmrUvpmQEoeE5CGlcvf5y33+NucED",
+  server: false
+
 config :core, GoEscuelaLms.Core.Repo,
   url: System.get_env("TEST_DATABASE_URL"),
   pool: Ecto.Adapters.SQL.Sandbox
