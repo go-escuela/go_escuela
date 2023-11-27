335565c4a6fbd5f25989b90717c80a18169b110b
nmenag <nmena.garzon@gmail.com>
2023-11-26 20:16:23 -0500

    Add web app

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,20 @@
 # move said applications out of the umbrella.
 import Config
 
+config :web,
+  generators: [context_app: false]
+
+# Configures the endpoint
+config :web, Web.Endpoint,
+  url: [host: "localhost"],
+  adapter: Phoenix.Endpoint.Cowboy2Adapter,
+  render_errors: [
+    formats: [json: Web.ErrorJSON],
+    layout: false
+  ],
+  pubsub_server: Web.PubSub,
+  live_view: [signing_salt: "Xzp7WliM"]
+
 config :core, :ecto_repos, [
   GoEscuelaLms.Core.Repo
 ]
