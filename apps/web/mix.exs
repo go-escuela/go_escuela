4ce981f66da85cb86db85f6d9298650faf34e70e
nmenag <nmena.garzon@gmail.com>
2023-12-01 23:37:58 -0500

    Add guardian DB

--- apps/web/mix.exs
+++ apps/web/mix.exs
@@ -44,6 +44,7 @@ defmodule Web.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:guardian, "~> 2.3"},
+      {:guardian_db, "~> 3.0"},
       {:core, in_umbrella: true}
     ]
   end
