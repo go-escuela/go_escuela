1ab44a70b518f0865900350768942be13b3ded81
nmenag <nmena.garzon@gmail.com>
2023-12-10 22:12:58 -0500

    Add institution info schema

---
+++ apps/core/priv/repo/migrations/20231211030921_create_institution_info.exs
@@ -0,0 +1,11 @@
+defmodule GoEscuelaLms.Core.Repo.Migrations.CreateInstitutionInfo do
+  use Ecto.Migration
+
+  def change do
+    create table("institution_info", primary_key: false) do
+      add :uuid, :uuid, primary_key: true
+      add :name, :string, null: false
+      timestamps()
+    end
+  end
+end
