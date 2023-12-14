d02d25b28210883ce0a13daf16f4c340ed99be01
nmenag <nmena.garzon@gmail.com>
2023-12-13 23:49:34 -0500

    Add is_permit_authorized plug

--- apps/web/lib/web/auth/authorized_plug.ex
+++ apps/web/lib/web/auth/authorized_plug.ex
@@ -5,15 +5,7 @@ defmodule Web.Auth.AuthorizedPlug do
   import Plug.Conn
   alias GoEscuelaLms.Core.Schema.User
 
-  # def is_authorized(%{params: %{"id" => id}} = conn, _opts) do
-  #   if conn.assigns.account.uuid == id do
-  #     conn
-  #   else
-  #     Web.FallbackController.call(conn, {:error, :forbidden}) |> halt()
-  #   end
-  # end
-
-  def is_admin_authorized(conn, _) do
+  def is_organizer_authorized(conn, _) do
     case conn.assigns.account |> User.organizer?() do
       true ->
         conn
@@ -25,6 +17,15 @@ defmodule Web.Auth.AuthorizedPlug do
 
   def is_instructor_authorized(conn, _) do
     case conn.assigns.account |> User.instructor?() do
+      true ->
+        conn
+      _ ->
+        Web.FallbackController.call(conn, {:error, :forbidden}) |> halt()
+    end
+  end
+
+  def is_permit_authorized(conn, _) do
+    case (conn.assigns.account |> User.instructor?() || conn.assigns.account |> User.organizer?() )  do
       true ->
         conn
 
