d0855e850cfbeac5ae994f9e6e8dbf06e0e66517
nmenag <nmena.garzon@gmail.com>
2024-03-13 22:31:21 -0500

    fix up CI

--- apps/core/lib/core/application.ex
+++ apps/core/lib/core/application.ex
@@ -7,18 +7,28 @@ defmodule Core.Application do
 
   @impl true
   def start(_type, _args) do
-    credentials = System.get_env("GOOGLE_API_CREDENTIALS") |> Base.decode64!() |> Jason.decode!()
-
-    source = {:service_account, credentials, []}
-
-    children = [
-      GoEscuelaLms.Core.Repo,
-      {Goth, name: Core.Goth, source: source}
-    ]
+    children =
+      Application.get_env(:core, :environment)
+      |> children()
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Core.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp children(:common) do
+    [
+      GoEscuelaLms.Core.Repo
+    ]
+  end
+
+  defp children(:test), do: children(:common)
+
+  defp children(_) do
+    credentials = System.get_env("GOOGLE_API_CREDENTIALS") |> Base.decode64!() |> Jason.decode!()
+    source = {:service_account, credentials, []}
+
+    children(:common) ++ [{Goth, name: Core.Goth, source: source}]
   end
 end
